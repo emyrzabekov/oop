b0c864112c160893aa86c451e50506ac0dba05d8
Your Name <you@example.com>
2021-05-04 19:59:12 +0600

    initial commit

---
+++ second_oop.py
@@ -0,0 +1,25 @@
+
+
+
+class Family:
+
+    father = "Erjan"
+    mother = "Meerim"
+    son = "Nurtegin"
+    daughter = "Telegey"
+
+
+    def havchik(self):
+        print("Kushat podano")
+
+    def spat(self):
+        print("Spim")
+
+family_at_home = Family()
+
+family_at_work = Family()
+
+
+print(family_at_home.havchik())
+
+print(family_at_work.spat())
